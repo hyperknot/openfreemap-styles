1902ad0344c84e5dab90af3b337ae126a75e9501
Zsolt Ero <zsolt.ero@gmail.com>
2024-01-04 23:37:30 +0100

    text-color

--- scripts/copy_text_fields.sh
+++ scripts/copy_text_fields.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-./copy_text_fields.py ../styles/bright/style.json ../styles/bright/maplibre.json
-./copy_text_fields.py ../styles/bright/style.json ../styles/liberty/style.json
-./copy_text_fields.py ../styles/bright/style.json ../styles/dark/style.json
-./copy_text_fields.py ../styles/bright/style.json ../styles/positron/style.json
+./copy_text_fields.py ../styles/liberty/style.json ../styles/bright/style.json
+./copy_text_fields.py ../styles/liberty/style.json ../styles/bright/maplibre.json
+./copy_text_fields.py ../styles/liberty/style.json ../styles/liberty/style.json
+./copy_text_fields.py ../styles/liberty/style.json ../styles/positron/style.json
+./copy_text_fields.py ../styles/liberty/style.json ../styles/dark/style.json
 
 cd ..
 ./lint.sh
