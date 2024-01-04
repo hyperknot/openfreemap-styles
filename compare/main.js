be5e1c7ba8eb8e2beea664065a4dc2a392f403a2
Zsolt Ero <zsolt.ero@gmail.com>
2024-01-04 23:14:53 +0100

    liberty landuse

--- compare/main.js
+++ compare/main.js
@@ -6,8 +6,11 @@ import { loadStyle } from './style.js'
 
 let left, right
 
-left = await loadStyle('/styles/bright/style.json', 'left')
-right = await loadStyle('/styles/bright/maphub.json', 'right')
+// left = await loadStyle('/styles/bright/style.json', 'left')
+// right = await loadStyle('/styles/bright/maphub.json', 'right')
+
+left = await loadStyle('/styles/liberty/style.json', 'left')
+right = await loadStyle('/styles/liberty/maphub.json', 'right')
 
 // left = await loadStyle('/dark/style.json', 'left')
 // right = await loadStyle('/dark/omt_orig_linted.json', 'right')
