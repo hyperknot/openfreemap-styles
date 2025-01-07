5b388c01b8ce490dd3de0f0f581e01c006138675
Zsolt Ero <zsolt.ero@gmail.com>
2025-01-07 17:05:49 +0100

    work

--- scripts/lint_styles.py
+++ scripts/lint_styles.py
@@ -57,13 +57,6 @@ def cli(styles_folder):
 
             print(p.stderr)
 
-            # # upgrade to MapLibre v21 styles
-            # p = subprocess.run(
-            #     ['node', 'scripts/upgrade_v21.mjs', style_file], capture_output=True, text=True
-            # )
-            # print(p.stdout)
-            # print(p.stderr)
-
             # prettier
             subprocess.run(
                 [
